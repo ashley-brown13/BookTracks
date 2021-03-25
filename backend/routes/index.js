287a4078869c323f070c06ec5bee677e891c51aa
Ashley Brown <ashleybrown101789@gmail.com>
2021-03-25 13:36:08 -0700

    phase one done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const csurf = require('csurf');
 const csrf = require('csurf');
-
+const apiRouter = require('./api');
 const csrfProtection = csrf({ cookie: true });
+router.use('/api', apiRouter);
+
+
 
 router.get('/hello/world', csrfProtection, function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
