85d804c78f82e93ed56954baf44716bc68ac7b08
Ashley Brown <ashleybrown101789@gmail.com>
2021-04-02 15:58:21 -0700

    seeder change

--- backend/db/seeders/20210331052921-default-playlists.js
+++ backend/db/seeders/20210331052921-default-playlists.js
@@ -9,8 +9,8 @@ module.exports = {
        "description": "Only the rainy songs",
        "spotifyLink": 'https://open.spotify.com/playlist/3J9YUqDKzAxaEGECSsg25L?si=CXKFMlNnReGsgZv5N3Ci9g',
        "imageURL": 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Old-black-appalachian-trail-tnnc1.jpg/1200px-Old-black-appalachian-trail-tnnc1.jpg',
-       "bookId": 120,
-       "userId": 10,
+       "bookId": 30,
+       "userId": 1,
        "createdAt": new Date(),
        "updatedAt": new Date()
       }
